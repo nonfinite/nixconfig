9c84bc43f1d384ead781444d1b9b90ecf906e6f7
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2023-11-02 12:57:40 -0600

    feat: added shell.nix to bootstrap flakes and git

---
+++ shell.nix
@@ -0,0 +1,23 @@
+# A shell with flake-enabled nix
+{ pkgs ? let
+    lock =
+      if builtins.pathExists ./flake.lock
+      then (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked
+      else { rev = "864814c092a7b9965104ce0b6639dc986350050b"; narHash = "sha256-HFjjlZY62hUdWYjnyRdAy7OoNChpC0zqiqVXAIcQFCw="; };
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in
+  import nixpkgs { overlays = [ ]; }
+, ...
+}:
+
+pkgs.mkShell {
+  NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+  nativeBuildInputs = with pkgs; [
+    git
+    nix
+    nixpkgs-fmt
+  ];
+}
