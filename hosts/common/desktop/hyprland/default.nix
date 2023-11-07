cb09bf5282f1e6aa8337e13b5329c69e72b97db8
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2023-11-06 20:17:54 -0700

    fix: enable networking

--- hosts/common/desktop/hyprland/default.nix
+++ hosts/common/desktop/hyprland/default.nix
@@ -2,6 +2,8 @@
 {
   programs.hyprland.enable = true;
 
+  networking.networkmanager.enable = true;
+
   # hyprland uses kitty as the default terminal, so we must ensure it's installed
   environment.systemPackages = with pkgs; [
     kitty
