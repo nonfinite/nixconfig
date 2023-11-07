5b6f5a68c5e1bc3e17e955b77aae05a40ec1e482
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2023-11-06 18:40:10 -0700

    feat: added dunst, polkit, xdg portal

--- home/nonfinite/features/desktop/hyprland/default.nix
+++ home/nonfinite/features/desktop/hyprland/default.nix
@@ -1,3 +1,12 @@
+{ pkgs, ... }:
 {
   home.file.".config/hypr/hyprland.conf".source = ../../../.config/hypr/hyprland.conf;
+
+  home.packages = with pkgs; [
+    dunst
+    libsForQt5.polkit-kde-agent
+    libsForQt5.qt5.qtwayland
+    qt6.qtwayland
+    xdg-desktop-portal-hyprland
+  ];
 }
