1f787b0077a956b233d6b1bbffe1dcd4dfa367b8
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2023-11-11 11:40:17 -0700

    refactor: replace  with SUPER

--- home/nonfinite/features/desktop/hyprland/binds.nix
+++ home/nonfinite/features/desktop/hyprland/binds.nix
@@ -1,83 +1,81 @@
 {
   wayland.windowManager.hyprland.settings = {
-    "$mainMod" = "SUPER";
-
     bind = [
       # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-      "$mainMod, Q, exec, kitty"
-      "$mainMod, W, killactive, "
-      "$mainMod, M, exit, "
-      "$mainMod, E, exec, nautilus"
-      "$mainMod, V, togglefloating,"
-      "$mainMod, P, pseudo,"
-      "$mainMod, S, togglesplit,"
+      "SUPER, Q, exec, kitty"
+      "SUPER, W, killactive, "
+      "SUPER, M, exit, "
+      "SUPER, E, exec, nautilus"
+      "SUPER, V, togglefloating,"
+      "SUPER, P, pseudo,"
+      "SUPER, S, togglesplit,"
 
       # Move focus with mainMod + arrow keys
-      "$mainMod, left, movefocus, l"
-      "$mainMod, right, movefocus, r"
-      "$mainMod, up, movefocus, u"
-      "$mainMod, down, movefocus, d"
+      "SUPER, left, movefocus, l"
+      "SUPER, right, movefocus, r"
+      "SUPER, up, movefocus, u"
+      "SUPER, down, movefocus, d"
 
       # Switch workspaces with mainMod + [0-9]
-      "$mainMod, 1, workspace, 1"
-      "$mainMod, 2, workspace, 2"
-      "$mainMod, 3, workspace, 3"
-      "$mainMod, 4, workspace, 4"
-      "$mainMod, 5, workspace, 5"
-      "$mainMod, 6, workspace, 6"
-      "$mainMod, 7, workspace, 7"
-      "$mainMod, 8, workspace, 8"
-      "$mainMod, 9, workspace, 9"
-      "$mainMod, 0, workspace, 10"
+      "SUPER, 1, workspace, 1"
+      "SUPER, 2, workspace, 2"
+      "SUPER, 3, workspace, 3"
+      "SUPER, 4, workspace, 4"
+      "SUPER, 5, workspace, 5"
+      "SUPER, 6, workspace, 6"
+      "SUPER, 7, workspace, 7"
+      "SUPER, 8, workspace, 8"
+      "SUPER, 9, workspace, 9"
+      "SUPER, 0, workspace, 10"
 
       # Move active window to a workspace with mainMod + SHIFT + [0-9]
-      "$mainMod SHIFT, 1, movetoworkspace, 1"
-      "$mainMod SHIFT, 2, movetoworkspace, 2"
-      "$mainMod SHIFT, 3, movetoworkspace, 3"
-      "$mainMod SHIFT, 4, movetoworkspace, 4"
-      "$mainMod SHIFT, 5, movetoworkspace, 5"
-      "$mainMod SHIFT, 6, movetoworkspace, 6"
-      "$mainMod SHIFT, 7, movetoworkspace, 7"
-      "$mainMod SHIFT, 8, movetoworkspace, 8"
-      "$mainMod SHIFT, 9, movetoworkspace, 9"
-      "$mainMod SHIFT, 0, movetoworkspace, 10"
+      "SUPER SHIFT, 1, movetoworkspace, 1"
+      "SUPER SHIFT, 2, movetoworkspace, 2"
+      "SUPER SHIFT, 3, movetoworkspace, 3"
+      "SUPER SHIFT, 4, movetoworkspace, 4"
+      "SUPER SHIFT, 5, movetoworkspace, 5"
+      "SUPER SHIFT, 6, movetoworkspace, 6"
+      "SUPER SHIFT, 7, movetoworkspace, 7"
+      "SUPER SHIFT, 8, movetoworkspace, 8"
+      "SUPER SHIFT, 9, movetoworkspace, 9"
+      "SUPER SHIFT, 0, movetoworkspace, 10"
 
       # Scroll through existing workspaces with mainMod + scroll
-      "$mainMod, mouse_down, workspace, e+1"
-      "$mainMod, mouse_up, workspace, e-1"
+      "SUPER, mouse_down, workspace, e+1"
+      "SUPER, mouse_up, workspace, e-1"
 
       # Move/resize windows with mainMod + SHIFT + HJKL
-      "$mainMod SHIFT, H, resizeactive, -10   0"
-      "$mainMod SHIFT, L, resizeactive,  10   0"
-      "$mainMod SHIFT, K, resizeactive,   0 -10"
-      "$mainMod SHIFT, J, resizeactive,   0  10"
+      "SUPER SHIFT, H, resizeactive, -10   0"
+      "SUPER SHIFT, L, resizeactive,  10   0"
+      "SUPER SHIFT, K, resizeactive,   0 -10"
+      "SUPER SHIFT, J, resizeactive,   0  10"
 
       #
       # Custom
       #
 
       # App Launcher
-      "$mainMod, R, exec, rofi -show drun"
-      "$mainMod, TAB, exec, rofi -show window"
+      "SUPER, R, exec, rofi -show drun"
+      "SUPER, TAB, exec, rofi -show window"
 
 
       # Move focus with mainMod + hjkl
-      "$mainMod, H, movefocus, l"
-      "$mainMod, L, movefocus, r"
-      "$mainMod, K, movefocus, u"
-      "$mainMod, J, movefocus, d"
+      "SUPER, H, movefocus, l"
+      "SUPER, L, movefocus, r"
+      "SUPER, K, movefocus, u"
+      "SUPER, J, movefocus, d"
 
       # Toggle fullscreen with bars
-      "$mainMod, F, fullscreen, 1"
+      "SUPER, F, fullscreen, 1"
 
       # Toggle true fullscreen
-      "$mainMod SHIFT, F, fullscreen, 0"
+      "SUPER SHIFT, F, fullscreen, 0"
     ];
 
     bindm = [
       # Move/resize windows with mainMod + LMB/RMB and dragging
-      "$mainMod, mouse:272, movewindow"
-      "$mainMod, mouse:273, resizewindow"
+      "SUPER, mouse:272, movewindow"
+      "SUPER, mouse:273, resizewindow"
     ];
   };
 }
