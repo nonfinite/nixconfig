bd94a408c0de7eb915e62cadc00e648f174bebb9
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2023-11-12 19:48:45 -0700

    feat: added swayidle

---
+++ home/nonfinite/features/desktop/hyprland/swayidle.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    swayidle
+  ];
+
+  wayland.windowManager.hyprland.settings.exec-once = [
+    "${pkgs.swayidle}/bin/swayidle"
+  ];
+
+  home.file.".config/swayidle/config".text = ''
+    timeout 300 "swaylock -f"
+    timeout 330 "hyprctl dispatch dpms off" resume "hyprctl dispatch dpms on"
+    timeout 10 "if pgrep -x swaylock; then hyprctl dispatch dpms off; fi" resume "hyprctl dispatch dpms on"
+  '';
+}
