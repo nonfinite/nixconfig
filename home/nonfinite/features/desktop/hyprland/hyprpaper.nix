fd1ac1fe304a1d9fa6ccb0c75c4b6bdca4f9ab63
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2023-11-19 13:54:20 -0700

    feat: switched wallpaper

--- home/nonfinite/features/desktop/hyprland/hyprpaper.nix
+++ home/nonfinite/features/desktop/hyprland/hyprpaper.nix
@@ -1,11 +1,15 @@
 { pkgs, ... }:
+let
+  # wp = "${../../../../../images/lakeside_sunset.png}";
+  wp = "${../../../../../images/firewatch.jpg}";
+in
 {
   home.packages = with pkgs; [
     hyprpaper
   ];
 
   home.file.".config/hypr/hyprpaper.conf".text = ''
-    preload = ${./img/lakeside_sunset.png}
-    wallpaper = ,${./img/lakeside_sunset.png}
+    preload = ${wp}
+    wallpaper = ,${wp}
   '';
 }
