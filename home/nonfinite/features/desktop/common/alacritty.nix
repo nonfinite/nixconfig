4ea20b8a00eb3e4ce7f43a5449c1b5ab255d83c8
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2023-11-27 19:17:31 -0700

    feat: added ability to choose alacritty theme

--- home/nonfinite/features/desktop/common/alacritty.nix
+++ home/nonfinite/features/desktop/common/alacritty.nix
@@ -1,10 +1,23 @@
-{ pkgs, ... }:
+{ ... }:
+let
+  themes = builtins.fetchGit {
+    url = "https://github.com/alacritty/alacritty-theme.git";
+    rev = "808b81b2e88884e8eca5d951b89f54983fa6c237";
+  };
+  # see themes at https://github.com/alacritty/alacritty-theme
+  theme-name = "citylights";
+in
 {
-  home.packages = with pkgs; [
-    alacritty
-  ];
-
   wayland.windowManager.hyprland.settings.windowrulev2 = [
     "group,class:Alacritty"
   ];
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      import = [
+        "${themes.outPath}/themes/${theme-name}.yaml"
+      ];
+    };
+  };
 }
