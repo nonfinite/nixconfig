6bc5ebdb859cef3ab11e3958e447edcf1cbcc81c
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2023-12-05 20:43:27 -0700

    feat: began configuring paperwm

--- home/nonfinite/common/desktop/gnome/default.nix
+++ home/nonfinite/common/desktop/gnome/default.nix
@@ -17,6 +17,28 @@
     "org/gnome/gnome-session" = {
       logout-prompt = false;
     };
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "paperwm@paperwm.github.com"
+      ];
+    };
+    "org/gnome/shell/extensions/paperwm" = {
+      window-gap = 0;
+      horizontal-margin = 0;
+      vertical-margin = 0;
+      vertical-margin-bottom = 0;
+    };
+    "org/gnome/shell/extensions/paperwm/keybindings" = {
+      close-window = [ "<Super>BackSpace" "<Super>w" ];
+      move-down-workspace = [ "<Shift><Super>k" ];
+      move-left = [ "<Shift><Super>h" ];
+      move-right = [ "<Shift><Super>l" ];
+      move-up-workspace = [ "<Shift><Super>j" ];
+      switch-down-workspace = [ "<Super>k" ];
+      switch-left = [ "<Super>h" ];
+      switch-right = [ "<Super>l" ];
+      switch-up-workspace = [ "<Super>j" ];
+    };
     "org/gtk/gtk4/settings/file-chooser" = {
       show-hidden = true;
       sort-directories-first = true;
