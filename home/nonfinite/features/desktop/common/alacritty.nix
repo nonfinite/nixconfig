e035aacf0213cfbcfd79fdbe827e356a544c7eb0
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2023-12-09 12:07:29 -0700

    fix: alacritty keybinding registers itself correctly

--- home/nonfinite/features/desktop/common/alacritty.nix
+++ home/nonfinite/features/desktop/common/alacritty.nix
@@ -30,7 +30,12 @@ in
   };
 
   dconf.settings = {
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/alacritty/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/alacritty" = {
       binding = "<Super>Return";
       command = "alacritty";
       name = "Alacritty";
