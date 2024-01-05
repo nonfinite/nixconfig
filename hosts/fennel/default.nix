7914f3001494b6d41a0ee8d34f3f86d02f640562
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-01-04 18:38:44 -0700

    feat: switch fennel to /dev/vda for virtual testing

--- hosts/fennel/default.nix
+++ hosts/fennel/default.nix
@@ -1,9 +1,8 @@
 args@{ inputs, lib, pkgs, ... }:
 let
   diskConfig = {
-    device = "/dev/disk/by-id/usb-Samsung_PSSD_T7_S5T3NJ0N902439F-0:0";
+    device = "/dev/vda";
     tmpfsSize = "2G";
-    keyFile = "/dev/disk/by-id/usb-USB_SanDisk_3.2Gen1_0501ae4193cfcc64f123d6180fa2025681512c89072972a58f03f0eaf8059da91385000000000000000000008b621e7a00081210835581075f2c8bb3-0:0";
   };
 in
 {
@@ -14,7 +13,6 @@ in
     (import ../common/boot/grub.nix (args // diskConfig))
 
     ../common/global
-    ../common/secrets
     ../common/zfs.nix
     ../common/virtualization/docker.nix
   ];
