e41cc9cc0d9ae3fc7b347fffb316e82c907f1fe9
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-01-07 14:52:08 -0700

    feat: mount tank on boot

--- hosts/fennel/zfs.nix
+++ hosts/fennel/zfs.nix
@@ -1,42 +1,9 @@
 { pkgs, ... }:
-let
-  zfs-init = pkgs.writeTextFile {
-    name = "zfs-init";
-    destination = "/bin/zfs-init";
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
-
-      if [[ $UID != 0 ]]; then
-        echo "Please run this script with sudo:"
-        echo "sudo $0 $*"
-        exit 1
-      fi
-
-      mkdir -p /etc/exports.d/
-
-      if [[ ! $(zpool list -Ho name | grep tank) ]]; then
-        echo "Importing tank..."
-        zpool import tank
-      fi
-
-      if [[ ! $(zfs list -Ho name | grep tank/enc) ]]; then
-        echo "Loading keys..."
-        zfs load-key tank/enc
-      fi
-
-      echo "Mounting..."
-      zfs mount -a
-    '';
-  };
-in
 {
   # zfs create -o mountpoint=/enc/ tank/enc
   imports = [
     ../common/zfs.nix
   ];
 
-  environment.systemPackages = [
-    zfs-init
-  ];
+  boot.zfs.extraPools = [ "tank" ];
 }
