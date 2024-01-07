c338f7c2e98c935df7d7ae601b49756910efdab1
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-01-06 21:48:06 -0700

    feat: enable zfs and podman

--- hosts/fennel/default.nix
+++ hosts/fennel/default.nix
@@ -16,8 +16,8 @@ in
     (import ../common/boot/network-luks-unlock.nix { networkKernelModule = "igb"; })
     ../common/global
     ../common/users/nonfinite
-    # ../common/zfs.nix
-    # ../common/virtualization/docker.nix
+    ../common/zfs.nix
+    ../common/virtualization/podman.nix
   ];
 
   networking = {
