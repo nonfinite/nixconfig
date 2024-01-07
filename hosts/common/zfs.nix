37b1cf87532d1f14dfc319647467fc997b92b334
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-01-07 11:29:00 -0700

    feat: added zfs-info command alias for easy summary

--- hosts/common/zfs.nix
+++ hosts/common/zfs.nix
@@ -6,4 +6,8 @@
 
   # networking.hostId must be set, generate a unique value with:
   # head -c4 /dev/urandom | od -A none -t x4
+
+  environment.shellAliases = {
+    "zfs-info" = "zfs list -o name,used,avail,refer,compressratio,encryptionroot,mountpoint,compression -s name";
+  };
 }
