fb53cd226d8e9b8043f5852976afd9deb1d0359a
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-01-07 14:02:31 -0700

    fix: ensure zfs-init run as sudo

--- hosts/fennel/zfs.nix
+++ hosts/fennel/zfs.nix
@@ -6,16 +6,26 @@ let
     executable = true;
     text = ''
       #!/usr/bin/env bash
+
+      if [[ $UID != 0 ]]; then
+        echo "Please run this script with sudo:"
+        echo "sudo $0 $*"
+        exit 1
+      fi
+
       mkdir -p /etc/exports.d/
 
       if [[ ! $(zpool list -Ho name | grep tank) ]]; then
+        echo "Importing tank..."
         zpool import tank
       fi
 
       if [[ ! $(zfs list -Ho name | grep tank/enc) ]]; then
+        echo "Loading keys..."
         zfs load-key tank/enc
       fi
 
+      echo "Mounting..."
       zfs mount -a
     '';
   };
