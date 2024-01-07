76bd114dfe645755c751a83d7ca0cbf9379e7a10
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-01-07 12:26:17 -0700

    feat: added init-zfs script

--- hosts/fennel/zfs.nix
+++ hosts/fennel/zfs.nix
@@ -1,5 +1,24 @@
+{ pkgs, ... }:
+let
+  init-zfs = pkgs.writeTextFile {
+    name = "init-zfs";
+    destination = "/bin/init-zfs";
+    executable = true;
+    text = ''
+      #!/usr/bin/env bash
+      mkdir /etc/exports.d/
+      zpool import tank && \
+      zfs load-key tank/enc && \
+      zfs mount -a
+    '';
+  };
+in
 {
   imports = [
     ../common/zfs.nix
+  ];
+
+  environment.systemPackages = [
+    init-zfs
   ];
 }
