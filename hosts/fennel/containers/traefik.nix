db80ff9146f117c8010064455447b6447cb331af
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-01-08 20:13:55 -0700

    feat: added TLS

--- hosts/fennel/containers/traefik.nix
+++ hosts/fennel/containers/traefik.nix
@@ -7,21 +7,44 @@
       cmd = [
         "--api=true"
         "--api.insecure=true"
+
+        "--log.level=INFO" # DEBUG, PANIC, FATAL, ERROR, WARN, INFO
+        "--log.filepath=/logs/traefik.log"
+
         "--global.sendAnonymousUsage=false"
         "--global.checkNewVersion=false"
+
         "--providers.docker=true"
         "--providers.docker.exposedbydefault=false"
+
         "--entrypoints.http.address=:80"
+        "--entrypoints.http.http.redirections.entryPoint.to=https"
+        "--entrypoints.http.http.redirections.entryPoint.scheme=https"
+
+        "--entrypoints.https.address=:443"
+        "--entrypoints.https.http.tls.certResolver=le-resolver"
+        "--entrypoints.https.http.tls.domains[0].main=${config.networking.domain}"
+        "--entrypoints.https.http.tls.domains[0].sans=*.${config.networking.domain}"
+
+        "--certificatesresolvers.le-resolver.acme.dnsChallenge.provider=duckdns"
+        "--certificatesresolvers.le-resolver.acme.email=acme@${config.networking.domain}"
+        "--certificatesresolvers.le-resolver.acme.storage=/letsencrypt/acme.json"
       ];
       dependsOn = [ ];
-      environmentFiles = [ ];
+      environmentFiles = [
+        # needs to contain DUCKDNS_TOKEN
+        "/enc/containers/traefik/.env"
+      ];
       labels = { };
       ports = [
         "80:80"
+        "443:443"
         "8080:8080"
       ];
       user = null;
       volumes = [
+        "/enc/containers/traefik/logs:/logs"
+        "/enc/containers/traefik/letsencrypt:/letsencrypt"
         "/var/run/docker.sock:/var/run/docker.sock:ro"
       ];
     };
@@ -32,7 +55,7 @@
       labels = {
         "traefik.enable" = "true";
         "traefik.http.routers.whoami.rule" = "Host(`whoami.${config.networking.domain}`)";
-        "traefik.http.routers.whoami.entrypoints" = "http";
+        "traefik.http.routers.whoami.entrypoints" = "https";
       };
     };
   };
