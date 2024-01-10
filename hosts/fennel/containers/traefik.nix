d8122e34664344d95b4338ef106a1df3af004cc0
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-01-09 19:49:23 -0700

    refactor: moved traefik config to yaml

--- hosts/fennel/containers/traefik.nix
+++ hosts/fennel/containers/traefik.nix
@@ -1,35 +1,78 @@
-{ config, ... }:
+{ config, pkgs, ... }:
+let
+  domain = config.networking.domain;
+
+  yaml = pkgs.formats.yaml { };
+
+  traefikConfig = {
+    api = {
+      insecure = true;
+    };
+
+    log = {
+      level = "INFO";
+      filepath = "/logs/traefik.log";
+    };
+
+    global = {
+      sendAnonymousUsage = false;
+      checkNewVersion = false;
+    };
+
+    providers = {
+      docker = {
+        exposedByDefault = false;
+      };
+    };
+
+    entrypoints = {
+      http = {
+        address = ":80";
+        http = {
+          redirections = {
+            entryPoint = {
+              to = "https";
+              scheme = "https";
+            };
+          };
+        };
+      };
+
+      https = {
+        address = ":443";
+        http = {
+          tls = {
+            certResolver = "letsencrypt";
+            domains = [
+              {
+                main = domain;
+                sans = "*.${domain}";
+              }
+            ];
+          };
+        };
+      };
+    };
+
+    certificatesResolvers = {
+      letsencrypt = {
+        acme = {
+          dnsChallenge.provider = "duckdns";
+          email = "acme@${domain}";
+          storage = "/letsencrypt/acme.json";
+        };
+      };
+    };
+  };
+
+  configFile = yaml.generate "traefik.yml" traefikConfig;
+in
 {
   virtualisation.oci-containers.containers = {
     traefik = {
       image = "docker.io/traefik:v2.10";
       hostname = "traefik";
-      cmd = [
-        "--api=true"
-        "--api.insecure=true"
-
-        "--log.level=INFO" # DEBUG, PANIC, FATAL, ERROR, WARN, INFO
-        "--log.filepath=/logs/traefik.log"
-
-        "--global.sendAnonymousUsage=false"
-        "--global.checkNewVersion=false"
-
-        "--providers.docker=true"
-        "--providers.docker.exposedbydefault=false"
-
-        "--entrypoints.http.address=:80"
-        "--entrypoints.http.http.redirections.entryPoint.to=https"
-        "--entrypoints.http.http.redirections.entryPoint.scheme=https"
-
-        "--entrypoints.https.address=:443"
-        "--entrypoints.https.http.tls.certResolver=le-resolver"
-        "--entrypoints.https.http.tls.domains[0].main=${config.networking.domain}"
-        "--entrypoints.https.http.tls.domains[0].sans=*.${config.networking.domain}"
-
-        "--certificatesresolvers.le-resolver.acme.dnsChallenge.provider=duckdns"
-        "--certificatesresolvers.le-resolver.acme.email=acme@${config.networking.domain}"
-        "--certificatesresolvers.le-resolver.acme.storage=/letsencrypt/acme.json"
-      ];
+      cmd = [ ];
       dependsOn = [ ];
       environmentFiles = [
         # needs to contain DUCKDNS_TOKEN
@@ -43,6 +86,7 @@
       ];
       user = null;
       volumes = [
+        "${configFile}:/etc/traefik/traefik.yml"
         "/enc/containers/traefik/logs:/logs"
         "/enc/containers/traefik/letsencrypt:/letsencrypt"
         "/var/run/docker.sock:/var/run/docker.sock:ro"
@@ -54,7 +98,7 @@
       hostname = "whoami";
       labels = {
         "traefik.enable" = "true";
-        "traefik.http.routers.whoami.rule" = "Host(`whoami.${config.networking.domain}`)";
+        "traefik.http.routers.whoami.rule" = "Host(`whoami.${domain}`)";
         "traefik.http.routers.whoami.entrypoints" = "https";
       };
     };
