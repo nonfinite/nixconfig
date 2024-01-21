7dc6292d4d7ad317198a98d168039dff5fbe7746
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-01-21 14:25:57 -0700

    feat: added youtube-archive service

--- hosts/mizuna/services/youtube-dl.nix
+++ hosts/mizuna/services/youtube-dl.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 let
   data = "/enc/data/Videos/YouTube";
+  yt-dlp = pkgs.unstable.yt-dlp;
+  imagemagick = pkgs.imagemagick;
   yt-dlp-conf = pkgs.writeText "yt-dlp.conf" ''
     -i
 
@@ -38,43 +40,64 @@ let
     --limit-rate 5M
     #--max-downloads 10
   '';
-  script = pkgs.writeTextFile
+  script = ''
+    pushd ${data}
+
+    echo 'Downloading new files for an hour...'
+    STOP=`date -d 'next hour' +%s`
+    while [[ $(date +%s) -lt $STOP ]]; do
+        fc1=$(find ${data} -name \*.* -print | wc -l)
+        ${yt-dlp}/bin/yt-dlp --config-location ${yt-dlp-conf} -a ${data}/.script/yt-dlp-channels.txt --max-downloads 10
+        fc2=$(find ${data} -name \*.* -print | wc -l)
+
+        for d in ${data}/*/; do
+            ${imagemagick}/bin/mogrify -format jpg "$d/*.webp" || true
+            bn=$(basename "$d")
+            mv "${data}/$bn/"*.webp "${data}/.meta/$bn/"
+        done
+
+        if [[ $fc1 -eq $fc2 ]]; then
+            break
+        fi
+    done
+
+    echo 'Downloading completed'
+
+    popd
+  '';
+  yt-archive = pkgs.writeTextFile
     {
       name = "yt-archive";
       executable = true;
       destination = "/bin/yt-archive";
       text = ''
         #!/usr/bin/env bash
-        pushd ${data}
-
-        echo 'Downloading new files for an hour...'
-        STOP=`date -d 'next hour' +%s`
-        while [[ $(date +%s) -lt $STOP ]]; do
-            fc1=$(find ${data} -name \*.* -print | wc -l)
-            yt-dlp --config-location ${yt-dlp-conf} -a ${data}/.script/yt-dlp-channels.txt --max-downloads 10
-            fc2=$(find ${data} -name \*.* -print | wc -l)
-
-            for d in ${data}/*/; do
-                mogrify -format jpg "$d/*.webp"
-                bn=$(basename "$d")
-                mv "${data}/$bn/"*.webp "${data}/.meta/$bn/"
-            done
-
-            if [[ $fc1 -eq $fc2 ]]; then
-                break
-            fi
-        done
-
-        echo 'Downloading completed'
-
-        popd
+        ${script}
       '';
     };
 in
 {
   environment.systemPackages = [
-    pkgs.unstable.yt-dlp
-    pkgs.imagemagick
-    script
+    yt-dlp
+    imagemagick
+    yt-archive
   ];
+
+  systemd.timers.youtube-archive = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      # every day at 11pm
+      OnCalendar = "*-*-* 23:00:00 America/Denver";
+      Persistent = true;
+    };
+  };
+
+  systemd.services.youtube-archive = {
+    script = script;
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+      User = "nonfinite";
+    };
+  };
 }
