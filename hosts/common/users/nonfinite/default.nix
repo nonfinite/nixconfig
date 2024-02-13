d0cd66df9686ae273ae1c66e493a9fefcbd3d01d
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-02-12 19:50:34 -0700

    feat: enable docker compat for podman

--- hosts/common/users/nonfinite/default.nix
+++ hosts/common/users/nonfinite/default.nix
@@ -17,6 +17,7 @@ in
         "libvirtd"
         "networkmanager"
         "onepassword-cli"
+        "podman"
         "vboxsf"
         "video"
       ];
--- hosts/common/virtualization/podman.nix
+++ hosts/common/virtualization/podman.nix
@@ -6,6 +6,9 @@
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
+
+      dockerCompat = true;
+      dockerSocket.enable = true;
     };
 
     oci-containers.backend = "podman";
