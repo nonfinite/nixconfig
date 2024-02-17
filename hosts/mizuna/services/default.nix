7c88e3f0f6c0a1d630cad7f28dc9ddb29756de3c
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-02-17 15:51:29 -0700

    feat: improved messages from update-containers script and run as root

--- hosts/mizuna/services/default.nix
+++ hosts/mizuna/services/default.nix
@@ -1,10 +1,17 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 let
   containers = config.virtualisation.oci-containers.containers;
-  containerNames = builtins.attrNames containers;
+  containerNames = lib.lists.sort (a: b: a < b) (lib.lists.unique (builtins.attrNames containers));
   lines = builtins.map
     (name: ''
+      if [[ $EUID -ne 0 ]]; then
+        echo "Script must be run as root, elevating..."
+        sudo "$0" "$@"
+        exit $?
+      fi
+
       if ! ${pkgs.docker}/bin/docker pull ${(builtins.getAttr name containers).image} | grep "Image is up to date"; then
+        echo "UPDATED: ${name}"
         systemctl restart docker-${name}.service
       fi
     '')
