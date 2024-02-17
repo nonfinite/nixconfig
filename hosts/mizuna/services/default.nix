f143f761b3e439a3f1d2fe789d30245fa2355655
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-02-16 20:40:54 -0700

    feat: added script to update containers

--- hosts/mizuna/services/default.nix
+++ hosts/mizuna/services/default.nix
@@ -1,3 +1,20 @@
+{ config, pkgs, ... }:
+let
+  containers = config.virtualisation.oci-containers.containers;
+  containerNames = builtins.attrNames containers;
+  lines = builtins.map
+    (name: ''
+      if ! ${pkgs.docker}/bin/docker pull ${(builtins.getAttr name containers).image} | grep "Image is up to date"; then
+        systemctl restart docker-${name}.service
+      fi
+    '')
+    containerNames;
+  update-containers = pkgs.writeShellScriptBin "update-containers" ''
+    export LANG=C
+
+    ${builtins.concatStringsSep "\n\n" lines}
+  '';
+in
 {
   imports = [
     ./authentik
@@ -9,8 +26,9 @@
     ./plex.nix
     ./prometheus.nix
     ./simmer.nix
-    # ./watchtower.nix
     ./youtube-dl.nix
     (import ./syncthing.nix { config = "/enc/containers"; data = "/enc/data"; })
   ];
+
+  environment.systemPackages = [ update-containers ];
 }
