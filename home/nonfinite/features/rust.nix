9db0b2effda4480c0c7252119d60ead6c983e850
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-05-10 21:22:23 -0600

    Revert "feat: removed rustup"
    This reverts commit 497eeb4079f4af3192d4358cfd5c026f8ce12a6d.

--- home/nonfinite/features/rust.nix
+++ home/nonfinite/features/rust.nix
@@ -1,9 +1,8 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  # prefer to add rust via individual dev shells/flakes to control different versions
-  # home.packages = with pkgs; [
-  #   rustup
-  # ];
+  home.packages = with pkgs; [
+    rustup
+  ];
 
   home.persistence."/nix/persist/home/nonfinite" = {
     directories = [
