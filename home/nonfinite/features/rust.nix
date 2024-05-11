497eeb4079f4af3192d4358cfd5c026f8ce12a6d
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-05-10 19:02:44 -0600

    feat: removed rustup

--- home/nonfinite/features/rust.nix
+++ home/nonfinite/features/rust.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  home.packages = with pkgs; [
-    rustup
-  ];
+  # prefer to add rust via individual dev shells/flakes to control different versions
+  # home.packages = with pkgs; [
+  #   rustup
+  # ];
 
   home.persistence."/nix/persist/home/nonfinite" = {
     directories = [
