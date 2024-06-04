313d65631c2068c654b1d39dbbc79a358123640e
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-06-03 20:57:12 -0600

    doc: added notes to cleanup script about recovering from 100% boot full

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 sudo nix-collect-garbage --delete-older-than 7d 
 nix-collect-garbage --delete-older-than 7d
+echo Note: must run `nixos-rebuild switch` before /boot will get cleaned up
+echo If /boot is full, try manually deleting the oldest kernel in /boot/kernels
+echo Use "df -h /boot" and "du -hs /boot/*" to check available space
