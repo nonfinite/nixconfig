2165633578473aa3d0285c663039950e8d844679
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-06-09 14:17:12 -0600

    fix: remove rustup which was causing conflicts with other flakes

--- home/nonfinite/features/rust.nix
+++ home/nonfinite/features/rust.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    rustup
-  ];
+  # home.packages = with pkgs; [
+  #   rustup
+  # ];
 
   home.persistence."/nix/persist/home/nonfinite" = {
     directories = [
