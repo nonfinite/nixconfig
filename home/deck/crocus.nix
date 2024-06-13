48de0b5568406ea5db007b4f2e2af1198369c4f1
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2024-06-12 21:25:50 -0600

    feat: enable syncthing

--- home/deck/crocus.nix
+++ home/deck/crocus.nix
@@ -1,16 +1,22 @@
 # Install with
-# ln -s /home/deck/.config/home-manager/home.nix
+# ln -s /home/deck/nixconfig/home/deck/crocus.nix /home/deck/.config/home-manager/home.nix
 { config, pkgs, ... }:
 
 {
-  
+
   home.username = "deck";
   home.homeDirectory = "/home/deck";
   home.stateVersion = "24.05";
 
-  home.packages = [];
-  home.sessionVariables = {};
+  home.packages = [ ];
+  home.sessionVariables = { };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # Syncthing
+  services.syncthing = {
+    enable = true;
+    tray = true;
+  };
 }
